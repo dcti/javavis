cfeeed4fe7925d51324b94c2b6cf200fe7c828f6
jlawson <jlawson>
2000-09-13 10:05:37 +0000

    updated makefiles

--- make.bat
+++ make.bat
@@ -1,7 +1,11 @@
 @echo off
+
+rem This is a batch file to compile the project using the Sun Java SDK.
+rem http://java.sun.com/j2se/1.3/
+
 if exist *.class del *.class
 if exist *.jar del *.jar
-javac -classpath . -sourcepath . RC5graph.java
+javac -classpath . RC5graph.java
 if errorlevel 1 goto end
 jar cvfm JavaVis.jar MANIFEST.MF *.class *.gif
 
---
+++ makems.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem This is a batch file to compile the project using the Microsoft SDK for Java compiler.
+rem http://www.microsoft.com/java/download/dl_sdk40.htm
+
+if exist *.class del *.class
+if exist *.jar del *.jar
+
+"D:\Program Files\Microsoft SDK for Java 4.0\Bin\jvc" /cp:p . RC5graph.java
+
+if errorlevel 1 goto end
+jar cvfm JavaVis.jar MANIFEST.MF *.class *.gif
+
+:end
